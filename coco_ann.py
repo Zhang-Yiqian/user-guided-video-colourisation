ea9909bb6a3fdbc1604df666ed6ceaeae93e15cf
Zhang-Yiqian <372451244@qq.com>
2020-02-24 11:15:03 +0000

    reshape

--- coco_ann.py
+++ coco_ann.py
@@ -7,30 +7,46 @@ Created on Sat Feb 22 16:58:58 2020
 """
 from cocoapi.PythonAPI.pycocotools.coco import COCO
 import numpy as np
-from skimage.io import imread, imshow
+from skimage.io import imread, imshow, imsave
 import matplotlib.pyplot as plt
 import pylab
-from skimage.morphology import skeletonize
+from skimage.morphology import skeletonize, thin, medial_axis
+from skimage import morphology
+from skimage import filters
+import skimage.filters.rank as sfr
+from skimage.morphology import disk
 
 annFile = '/home/yiqian/Documents/dataset/COCO/annotations_valstuff/stuff_val2017.json'
 coco=COCO(annFile)
 imgIds = coco.getImgIds()
 img = coco.loadImgs(imgIds[0])[0]
 I = imread(img['coco_url'])       
-plt.figure()
-imshow(I)
+# plt.figure()
+# imshow(I)
 catIds = coco.getCatIds(catNms=['person']);
 # annIds = coco.getAnnIds(imgIds=img['id'], catIds=coco.getCatIds(), iscrowd=False)
 annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=False)
 anns = coco.loadAnns(annIds)
-plt.figure()
-coco.showAnns(anns)
+# plt.figure()
+# coco.showAnns(anns)
+
+# sample scribble areas based on uniform distribution
+# sample_number = np.random.randint(1, len(anns)+1)
+sample_number = 4
+area_list = np.random.permutation(len(anns)+1)[0 : sample_number+1]
+kernel = morphology.square(5)
 scribbles = np.zeros([I.shape[0], I.shape[1]])
-for i in range(len(anns)):
-    mask = coco.annToMask(anns[i]) * 1.0
-    scribbles += skeletonize(mask)
 plt.figure()
-imshow(scribbles)
+for i in range(len(anns)):
+    mask = coco.annToMask(anns[i])
+    skel = skeletonize(mask) * 1.0
+    dst = sfr.gradient(skel, disk(5))
+    scribbles += (dst > 0.5) * 1.0
+    # scribbles += skel
+    # scribbles += morphology.opening(skel, kernel) 
+
+plt.figure()
+imshow((scribbles> 0.5) * 1.0)
 
 
 
