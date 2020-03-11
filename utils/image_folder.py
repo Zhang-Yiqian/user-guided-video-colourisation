1a539ab9ca47a165091a64d8594fdd84ef03852a
Zhang-Yiqian <372451244@qq.com>
2020-03-11 17:32:20 +0000

    .

--- utils/image_folder.py
+++ utils/image_folder.py
@@ -37,7 +37,7 @@ class ImageFolder(data.Dataset):
 
     def __init__(self, root, transform=None, return_paths=False,
                  loader=default_loader):
-        imgs = make_dataset(root)
+        # imgs = make_dataset(root)
         if len(imgs) == 0:
             raise(RuntimeError("Found 0 images in: " + root + "\n"
                                "Supported image extensions are: " +
